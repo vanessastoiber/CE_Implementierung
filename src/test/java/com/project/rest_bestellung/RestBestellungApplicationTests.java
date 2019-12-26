e8171f90a9ad687ae53e4e948b47d3768561304b
vanessastoiber <vanessa.stoiber@gmail.com>
2019-12-26 19:58:46 +0100

    initial commit for Consuming a RESTful Web Service

---
+++ src/test/java/com/project/rest_bestellung/RestBestellungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.rest_bestellung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBestellungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
