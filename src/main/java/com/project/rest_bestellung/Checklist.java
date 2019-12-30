b31595a6d79a231816ddec1d898b794d41143ac6
vanessastoiber <vanessa.stoiber@gmail.com>
2019-12-30 12:38:36 +0100

    add template and handling for selection of handlebar type

---
+++ src/main/java/com/project/rest_bestellung/Checklist.java
@@ -0,0 +1,15 @@
+package com.project.rest_bestellung;
+
+import java.util.List;
+
+public class Checklist {
+    private List<String> checkedItems;
+
+    public List<String> getCheckedItems() {
+        return checkedItems;
+    }
+
+    public void setCheckedItems(List<String> checkedItems) {
+        this.checkedItems = checkedItems;
+    }
+}
--- src/main/java/com/project/rest_bestellung/ProduktController.java
+++ src/main/java/com/project/rest_bestellung/ProduktController.java
@@ -3,9 +3,16 @@ package com.project.rest_bestellung;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller to test RestTemplate configuration
@@ -29,11 +36,36 @@ public class ProduktController {
     }
 
     @RequestMapping("/produkt/lenkertyp")
-    public String getAllHandleBarTypes() {
+    public ArrayList getAllHandleBarTypes() {
         final String RESOURCE_URL = "https://www.maripavi.at/produkt/lenkertyp";
-        ResponseEntity<String> response = restTemplate.getForEntity(RESOURCE_URL, String.class);
+        ResponseEntity<ArrayList> response = restTemplate.getForEntity(RESOURCE_URL, ArrayList.class);
 
         return response.getBody();
+    }
+
+    @RequestMapping(value = "/produkt/lenkertyp", method= RequestMethod.GET)
+    public ModelAndView showForm(Model model) {
+        ArrayList result = getAllHandleBarTypes();
+        List<String> allItems = new ArrayList<String>();
+        allItems.add((String) result.get(0));
+        allItems.add((String) result.get(1));
+        allItems.add((String) result.get(2));
+        model.addAttribute("allItems", allItems);
+
+        Checklist checklist = new Checklist();
+        List<String> checkedItems = new ArrayList<String>();
+        // first value will be checked by default.
+        checkedItems.add((String) result.get(0));
+        checklist.setCheckedItems(checkedItems);
+        model.addAttribute("checklist", checklist);
+        return new ModelAndView("checklistForHandlebarType");
+    }
+
+    @RequestMapping(value = "/processForm", method=RequestMethod.POST)
+    public void processForm(@ModelAttribute(value="checklistForHandlebarType") Checklist checklist) {
+        // Get value of checked item.
+        List<String> checkedItems = checklist.getCheckedItems();
+        System.out.println(checkedItems);
     }
 
     @RequestMapping("/produkt/material")
