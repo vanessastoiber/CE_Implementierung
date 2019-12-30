86411d65fe17d2879ca0600aa3b5f0031e74ebb5
Bernadette Elena Hammerle <bernadette.elena.hammerle@gmail.com>
2019-12-30 20:43:19 +0100

    customer: changed form to thymeleaf; return to form if input is invalid

--- src/main/java/com/project/rest_bestellung/CustomerController.java
+++ src/main/java/com/project/rest_bestellung/CustomerController.java
@@ -1,40 +1,30 @@
 package com.project.rest_bestellung;
+import javax.validation.Valid;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import javax.validation.constraints.Size;
-
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@RestController
-@Validated
-public class CustomerController {
+@Controller
+public class CustomerController {	
+	@RequestMapping(value="/customer", method=RequestMethod.POST)
+    public String checkCustomerInfo(@Valid User customerForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+        	model.addAttribute("customerForm", customerForm);
+            return "createCustomer";
+        }
 
-	private final AtomicLong nextId = new AtomicLong();
-	
-    @RequestMapping("/customer/create")
-    public User create(@RequestParam @Size(min=2, max=64) String firstName, 
-    		@RequestParam @Size(min=2, max=64) String lastName,
-    		@RequestParam(required=false) String emailAddress,
-    		@RequestParam(required=false) String username,
-    		@RequestParam(required=false) String deliveryAddress,
-    		@RequestParam(required=false) String invoiceAddress) {
-    	long id = nextId.incrementAndGet();
-    	User user = new User(id, emailAddress, firstName, lastName, username, deliveryAddress, invoiceAddress);
-        
-        return user;
+        return "redirect:/produkt/lenkertyp";
     }
     
-    @GetMapping("/customer/form")
-    public ModelAndView customerForm() {
-    	return new ModelAndView("createCustomer");
+    @RequestMapping(value="/customer", method=RequestMethod.GET)
+    public ModelAndView showCustomerForm(Model model) {
+        model.addAttribute("customerForm", new User());
+        return new ModelAndView("createCustomer");
     }
 
-    
 }
--- src/main/java/com/project/rest_bestellung/User.java
+++ src/main/java/com/project/rest_bestellung/User.java
@@ -1,77 +1,104 @@
 package com.project.rest_bestellung;
 
-public class User {
-    private Long id;
-    private String email;
-    private String vorname;
-    private String nachname;
-    private String username;
-    private String lieferadresse;
-    private String rechnungsadresse;
+import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class User {
+    @NotNull
+    @Size(min=2, max=30)
+    private String firstName;
+    @NotNull
+    @Size(min=2, max=30)
+    private String lastName;
+    
+    private Long id;
+    private String emailAddress;
+    private String username;
+    private String deliveryAddress;
+    private String invoiceAddress;
+    
+    public User() {
+    	
+    }
+    
     public User(Long id, String email, String vorname, String nachname, String username, String lieferadresse, String rechnungsadresse) {
         this.id = id;
-        this.email = email;
-        this.vorname = vorname;
-        this.nachname = nachname;
+        this.emailAddress = email;
+        this.firstName = vorname;
+        this.lastName = nachname;
         this.username = username;
-        this.lieferadresse = lieferadresse;
-        this.rechnungsadresse = rechnungsadresse;
+        this.deliveryAddress = lieferadresse;
+        this.invoiceAddress = rechnungsadresse;
     }
-
+    
     public Long getId() {
-        return this.id;
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public String getInvoiceAddress() {
+		return invoiceAddress;
+	}
+
+	public void setInvoiceAddress(String invoiceAddress) {
+		this.invoiceAddress = invoiceAddress;
+	}
+
+	private List<String> checkedItems;
+
+    public List<String> getCheckedItems() {
+        return checkedItems;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCheckedItems(List<String> checkedItems) {
+        this.checkedItems = checkedItems;
     }
 
-    public String getEmail() {
-        return this.email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getVorname() {
-        return this.vorname;
-    }
-
-    public void setVorname(String vorname) {
-        this.vorname = vorname;
-    }
-
-    public String getNachname() {
-        return this.nachname;
-    }
-
-    public void setNachname(String nachname) {
-        this.nachname = nachname;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getLieferadresse() {
-        return this.lieferadresse;
-    }
-
-    public void setLieferadresse(String lieferadresse) {
-        this.lieferadresse = lieferadresse;
-    }
-
-    public String getRechnungsadresse() {
-        return this.rechnungsadresse;
-    }
-
-    public void setRechnungsadresse(String rechnungsadresse) {
-        this.rechnungsadresse = rechnungsadresse;
-    }
+    
 }
