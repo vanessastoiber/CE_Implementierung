3285279969c2cb5603d521829442d35ed4e4d838
Bernadette Elena Hammerle <bernadette.elena.hammerle@gmail.com>
2020-01-15 13:01:27 +0100

    redirect urls, catch order error

--- src/main/java/com/project/rest_bestellung/ProduktController.java
+++ src/main/java/com/project/rest_bestellung/ProduktController.java
@@ -8,11 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Controller to test RestTemplate configuration
@@ -52,34 +55,59 @@ public class ProduktController {
     }
     
     @RequestMapping(value = "/processHandleForm", method= RequestMethod.POST)
-    public ModelAndView processHandleForm(@ModelAttribute(value="checklistForHandle") Checklist checklist, Model model) {
+    public String processHandleForm(@ModelAttribute(value="checklistForHandle") Checklist checklist, Model model) {
         // Get value of checked item.
         List<String> checkedItems = checklist.getCheckedItems();
         System.out.println("ausgewählter Griff: " + checkedItems);
         order.setHandle(checkedItems.get(0));
         System.out.println(order.toString());
-        
-        model = postOrder(model);
-        
-        return new ModelAndView("order");
+
+        return "redirect:/order";
     }
     
-    public Model postOrder(Model model)  {  
-        model.addAttribute("handlebartype", order.getHandlebartype());
-        model.addAttribute("material", order.getMaterial());
-        model.addAttribute("gearLevel", order.getGearLevels());
-        model.addAttribute("handle", order.getHandle());
+    @RequestMapping(value = "/order", method=RequestMethod.GET)
+    public ModelAndView postOrder(Model model)  {  
+    	String handlebartype = order.getHandlebartype();
+    	String material = order.getMaterial();
+    	String gearLevels = order.getGearLevels();
+    	String handle = order.getHandle();
+    	
+        model.addAttribute("handlebartype", handlebartype);
+        model.addAttribute("material", material);
+        model.addAttribute("gearLevel", gearLevels);
+        model.addAttribute("handle", handle);
         
-        String sendOrderUrl = "https://www.maripavi.at/bestellung" + 
-        		"?lenkertyp=" + order.getHandlebartype() +
-        		"&material=" + order.getMaterial() +
-        		"&schaltung=" + order.getGearLevels() +
-        		"&griff=" + order.getHandle();
+        String sendOrderUrl = "https://www.maripavi.at/bestellung";
         
-	    ResponseEntity<String> response = restTemplate.postForEntity(sendOrderUrl, "", String.class);
-        model.addAttribute("responseBody", response.getBody());
+        if (handlebartype!=null){
+        	sendOrderUrl += "?lenkertyp=" + handlebartype;
+        }
+        if (material!=null){
+        	sendOrderUrl += "&material=" + material;
+        }
+        if (gearLevels!=null){
+        	sendOrderUrl += "&schaltung=" + gearLevels;
+        }
+        if (handle!=null){
+        	sendOrderUrl += "&griff=" + handle;
+        }
         
-        return model;
+        System.out.println(sendOrderUrl);
+        
+        try{
+		    ResponseEntity<String> response = restTemplate.postForEntity(sendOrderUrl, "", String.class);
+		    model.addAttribute("responseBody", response.getBody());
+        }catch(HttpClientErrorException e){
+        	Pattern pattern = Pattern.compile("detail\":\"(.*?)\"");
+        	Matcher matcher = pattern.matcher(e.getResponseBodyAsString());
+            if(matcher.find()) {
+                model.addAttribute("error", "Bestellung fehlgeschlagen, " + matcher.group(1));
+            }else {
+            	model.addAttribute("error", "Bestellung fehlgeschlagen");
+            }
+        }
+        
+        return new ModelAndView("order");
     }
 
     @RequestMapping("/produkt/lenkertyp")
@@ -109,12 +137,12 @@ public class ProduktController {
     }
 
     @RequestMapping(value = "/processForm", method=RequestMethod.POST)
-    public ModelAndView processForm(@ModelAttribute(value="checklistForHandlebarType") Checklist checklist, Model model) {
+    public String processForm(@ModelAttribute(value="checklistForHandlebarType") Checklist checklist, Model model) {
         // Get value of checked item.
         List<String> checkedItems = checklist.getCheckedItems();
         order.setHandlebartype(checkedItems.get(0));
         System.out.println(order.toString());
-        return showFormMaterial(model);
+        return "redirect:/produkt/material";
     }
 
     @RequestMapping("/produkt/material")
@@ -140,13 +168,13 @@ public class ProduktController {
     }
     
     @RequestMapping(value = "/processMaterialForm", method= RequestMethod.POST)
-    public ModelAndView processMaterialForm(@ModelAttribute(value="checklistForMaterial") Checklist checklist, Model model) {
+    public String processMaterialForm(@ModelAttribute(value="checklistForMaterial") Checklist checklist, Model model) {
         // Get value of checked item.
         List<String> checkedItems = checklist.getCheckedItems();
         System.out.println("ausgewählter Lenkertyp: " + checkedItems);
         order.setMaterial(checkedItems.get(0));
         System.out.println(order.toString());
-        return showFormGearLevel(model);
+        return "redirect:/produkt/schaltung";
     }
     
     @RequestMapping("/produkt/schaltung")
@@ -172,12 +200,12 @@ public class ProduktController {
     }
     
     @RequestMapping(value = "/processGearLevelForm", method= RequestMethod.POST)
-    public ModelAndView processGearLevelForm(@ModelAttribute(value="checklistForGearLevels") Checklist checklist, Model model) {
+    public String processGearLevelForm(@ModelAttribute(value="checklistForGearLevels") Checklist checklist, Model model) {
         // Get value of checked item.
         List<String> checkedItems = checklist.getCheckedItems();
         System.out.println("ausgewählte Schaltung: " + checkedItems);
         order.setGearLevels(checkedItems.get(0));
         System.out.println(order.toString());
-        return showFormHandles(model);
+        return "redirect:/produkt/griff";
     }
 }
